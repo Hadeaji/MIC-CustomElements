54d8cc57f5faf85bf91c0abd6bae1542d9e418ad
hadeaji <hadeaji@gmail.com>
2022-09-18 12:13:45 +0300

    quick adjustment

--- githubCDN/v1/mi-billing-payment.js
+++ githubCDN/v1/mi-billing-payment.js
@@ -5,6 +5,10 @@ const miTempBillingPaymentTemplateString =
   font-family: system-ui;
 }
 
+h3 {
+  text-align: left;
+}
+
 .card {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
